e8a66363b76cdc013c3c691477d73196eca18f8e
polarbear430 <1525625981@qq.com>
2018-11-14 14:14:00 +0800

    分页

--- src/main/java/com/viol3t/tmall_springboot/service/CategoryService.java
+++ src/main/java/com/viol3t/tmall_springboot/service/CategoryService.java
@@ -2,7 +2,11 @@ package com.viol3t.tmall_springboot.service;
 
 import com.viol3t.tmall_springboot.dao.CategoryDAO;
 import com.viol3t.tmall_springboot.pojo.Category;
+import com.viol3t.tmall_springboot.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +16,14 @@ import java.util.List;
 public class CategoryService {
     @Autowired
     CategoryDAO categoryDAO;
+
+    public Page4Navigator<Category> list(int start,int size,int navigatePages){
+        Sort sort = new Sort(Sort.Direction.DESC,"id");
+        Pageable pageable = new PageRequest(start,size,sort);
+        Page pageFromJPA = categoryDAO.findAll(pageable);
+        return new Page4Navigator<>(pageFromJPA,navigatePages);
+    }
+
     public List<Category> list(){
         Sort sort = new Sort(Sort.Direction.DESC,"id");
         return categoryDAO.findAll(sort);
--- src/main/java/com/viol3t/tmall_springboot/web/CategoryController.java
+++ src/main/java/com/viol3t/tmall_springboot/web/CategoryController.java
@@ -2,8 +2,10 @@ package com.viol3t.tmall_springboot.web;
 
 import com.viol3t.tmall_springboot.pojo.Category;
 import com.viol3t.tmall_springboot.service.CategoryService;
+import com.viol3t.tmall_springboot.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,7 +16,10 @@ public class CategoryController {
     CategoryService categoryService;
 
     @GetMapping("/categories")
-    public List<Category> list(){
-        return categoryService.list();
+    public Page4Navigator<Category> list(@RequestParam(value = "start",defaultValue = "0") int start,
+                                         @RequestParam(value = "size",defaultValue = "5") int size)throws Exception{
+        start = start<0?0:start;
+        Page4Navigator<Category> page = categoryService.list(start,size,5);
+        return page;
     }
 }
