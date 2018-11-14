361737a9c1909954f4431e4b36622bd0416fb4df
polarbear430 <1525625981@qq.com>
2018-11-14 14:41:00 +0800

    增加分类，上传图片

--- src/main/java/com/viol3t/tmall_springboot/web/CategoryController.java
+++ src/main/java/com/viol3t/tmall_springboot/web/CategoryController.java
@@ -2,12 +2,20 @@ package com.viol3t.tmall_springboot.web;
 
 import com.viol3t.tmall_springboot.pojo.Category;
 import com.viol3t.tmall_springboot.service.CategoryService;
+import com.viol3t.tmall_springboot.util.ImageUtil;
 import com.viol3t.tmall_springboot.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -21,5 +29,21 @@ public class CategoryController {
         start = start<0?0:start;
         Page4Navigator<Category> page = categoryService.list(start,size,5);
         return page;
+    }
+
+    @PostMapping("/categories")
+    public Object add(Category bean, MultipartFile image, HttpServletRequest request)throws Exception{
+        categoryService.add(bean);
+        saveOrUpdateImageFile(bean, image, request);
+        return bean;
+    }
+    public void saveOrUpdateImageFile(Category bean,MultipartFile image,HttpServletRequest request)throws IOException{
+        File imageFolder = new File(request.getServletContext().getRealPath("img/category"));
+        File file = new File(imageFolder,bean.getId()+".jpg");
+        if(!file.getParentFile().exists())
+            file.getParentFile().mkdirs();
+        image.transferTo(file);
+        BufferedImage img = ImageUtil.change2jpg(file);
+        ImageIO.write(img,"jpg",file);
     }
 }
