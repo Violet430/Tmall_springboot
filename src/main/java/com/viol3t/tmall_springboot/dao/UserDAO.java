67ece419d367bc030a0edfe6d0867d86cfd62df9
polarbear430 <1525625981@qq.com>
2018-11-16 15:00:04 +0800

    后台用户管理

---
+++ src/main/java/com/viol3t/tmall_springboot/dao/UserDAO.java
@@ -0,0 +1,7 @@
+package com.viol3t.tmall_springboot.dao;
+
+import com.viol3t.tmall_springboot.pojo.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDAO extends JpaRepository<User,Integer> {
+}
